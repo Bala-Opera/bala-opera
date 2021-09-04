b598c53049becdedc28e3dced160f2c993e0ac42
daniellehu <hu.daniellh@gmail.com>
2021-09-04 01:24:11 -0400

    adding assets, update server.ts for deployment

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'http://localhost:3000';
+export const server = dev ? 'http://localhost:3000' : 'https://bala-opera.vercel.app';
