b8d7a2a2ccd13d18dac424fd2834864edd4e2e1d
daniellehu <hu.daniellh@gmail.com>
2021-10-09 14:34:28 -0400

    adding button hover and disabled state

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -4,15 +4,18 @@ import styles from './button.module.scss';
 export default function Button({
   text,
   isImportant,
+  isDisabled,
   clickHandler,
 }: {
   text: string,
   isImportant?: boolean,
+  isDisabled?: boolean,
   clickHandler: MouseEventHandler,
 }) {
+  const buttonStyle = isDisabled ? styles.disabled : styles.button;
   const background = isImportant ? styles.important : styles.default;
   return (
-    <button className={`${background} ${styles.button}`} onClick={clickHandler}>
+    <button disabled={isDisabled} className={`${background} ${buttonStyle}`} onClick={clickHandler}>
       {text}
     </button>
   )
