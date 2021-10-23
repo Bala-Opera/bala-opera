486c42f4febc6c4c15bb18faf6fb64dde2194e84
daniellehu <hu.daniellh@gmail.com>
2021-10-23 15:40:00 -0400

    adding correct hover state for minimize icon and replacing - with icon

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,6 +1,11 @@
 import { MouseEventHandler } from 'react';
 import styles from './header.module.scss'
 
+const MinimizeIcon = () => (
+<svg width="16" height="3" viewBox="0 0 16 3" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.minimizeIcon}>
+  <path d="M0.046875 0.25V2.754H15.9569V0.25H0.046875Z" />
+</svg>)
+
 export default function Header({
   title,
   minimizeHandler,
@@ -12,7 +17,9 @@ export default function Header({
   return (
     <div className={styles.header}>
       <p className={styles.text}>{title}</p>
-      <button className={styles.minimize} onClick={minimizeHandler}>-</button>
+      <button className={styles.minimize} onClick={minimizeHandler}>
+        <MinimizeIcon />
+      </button>
     </div>
   );
 }
